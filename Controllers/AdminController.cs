2ed3d1dcad7bf7de8e2ba0f7f7e88c1d2290f184
khoinguyen80 <59272921+khoinguyen80@users.noreply.github.com>
2021-10-25 15:42:01 +0700

    admin- Done user-Staff-Create

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,17 +1,162 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AppDevelopment0805.Models;
+using AppDevelopment0805.Roles;
+using AppDevelopment0805.ViewModels;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
 namespace AppDevelopment0805.Controllers
 {
+    [Authorize(Roles = Role.Admin)]
     public class AdminController : Controller
     {
+        private ApplicationDbContext _context;
+        private ApplicationUserManager _userManager;
+        private ApplicationSignInManager _signInManager;
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        public AdminController(ApplicationUserManager userManager)
+        {
+            UserManager = userManager;
+            _context = new ApplicationDbContext();
+        }
+        public ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+        public ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddStaff()
+        {
+            var staffs = _context.Staffs.ToList();
+            return View(staffs);
+        }
+
+        [HttpGet]
+        public ActionResult CreateStaff()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateStaff(CreateStaffViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = viewModel.Email, Email = viewModel.Email };
+                var result = await UserManager.CreateAsync(user, viewModel.Password);
+                var StaffId = user.Id;
+                var newStaff = new Staff()
+                {
+                    Email = viewModel.Email,
+                    StaffId = StaffId,
+                    Name = viewModel.Name,
+                    Age = viewModel.Age,
+                    Address = viewModel.Address
+                };
+                if (result.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(user.Id, Role.Staff);
+                    _context.Staffs.Add(newStaff);
+                    _context.SaveChanges();
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("AddStaff", "Admin");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(viewModel);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult CreateTrainer()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateTrainer(CreateTrainerViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = viewModel.Email, Email = viewModel.Email };
+                var result = await UserManager.CreateAsync(user, viewModel.Password);
+                var TrainerId = user.Id;
+                var newTrainer = new Trainer()
+                {
+                    TrainerId = TrainerId,
+                    Email = viewModel.Email,
+                    Name = viewModel.Name,
+                    Age = viewModel.Age,
+                    Address = viewModel.Address,
+                    Specialty = viewModel.Specialty
+                };
+                if (result.Succeeded)
+                {
+                    await UserManager.AddToRoleAsync(user.Id, Role.Trainer);
+                    _context.Trainers.Add(newTrainer);
+                    _context.SaveChanges();
+
+                    // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("AddTrainer", "Admin");
+                }
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(viewModel);
         }
     }
 }
