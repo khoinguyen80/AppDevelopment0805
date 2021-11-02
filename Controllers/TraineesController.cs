c6db4afaa29fd3ec843201eb7f3167dc698e50f1
khoinguyen80 <59272921+khoinguyen80@users.noreply.github.com>
2021-11-02 14:15:41 +0700

    add-traineecontroller

--- Controllers/TraineesController.cs
+++ Controllers/TraineesController.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using AppDevelopment0805.Models;
+using AppDevelopment0805.Roles;
+using Microsoft.AspNet.Identity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AppDevelopment0805.Controllers
 {
+    [Authorize(Roles = Role.Trainee)]
     public class TraineesController : Controller
     {
-        // GET: Trainees
+        private ApplicationDbContext _context;
+        public TraineesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Trainee
         public ActionResult Index()
         {
-            return View();
+            var userId = User.Identity.GetUserId();
+            var traineeInDb = _context.Trainees
+                .SingleOrDefault(t => t.TraineeId == userId);
+            return View(traineeInDb);
+        }
+
+        [HttpGet]
+        public ActionResult Courses()
+        {
+            var userId = User.Identity.GetUserId();
+
+            var courses = _context.TraineesCourses.Where(t => t.Trainee.TraineeId == userId).Select(t => t.Course).ToList();
+            return View(courses);
+        }
+
+        [HttpGet]
+        public ActionResult CourseTrainees(int id)
+        {
+            var traineesCourse = _context.TraineesCourses.Where(t => t.CourseId == id).Select(t => t.Trainee).ToList();
+            return View(traineesCourse);
         }
     }
 }
